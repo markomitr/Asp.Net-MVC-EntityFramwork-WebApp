22f45bee3413c719d524612a870eb340aca863cf
markomitr <marko.mitreski@gmail.com>
2015-10-27 15:21:04 -0600

    Web App

---
+++ TeslaSolarWeb/ViewModel/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeslaSolarWeb.ViewModel
+{
+    public class EmailMsg
+    {
+        [Required,DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required,DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+
+        public bool IsSent { get; set; }
+
+        public string Poraka { get; set; }
+        public string ErrorMsg { get; set; }
+    }
+}
---
+++ TeslaSolarWeb/ViewModel/PrictureEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TeslaSolarWeb.ViewModel
+{
+    public class PrictureEdit
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        [DisplayName("Image")]
+         public string ImageUrl { get; set; }
+         public string ImageUrlSmall { get; set; }
+    }
+}
